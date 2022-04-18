f39744bfeeafe7c29c58585abe7760d1361687ab
tawanchaiii <tawanchai_champ@hotmail.com>
2022-04-18 16:53:00 +0700

    language metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'tawanchaiii', // Usually your GitHub org/user name.
   projectName: 'blog.tawanchaiii.tk', // Usually your repo name.
 
   presets: [
